19d7d453168e629096f16feef9abeb6deb066e46
Elias Howell <ejh@snarf.top>
2021-02-07 13:55:54 -0600

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Install prerequisites for germination.py
-sudo apt install python3 python3-pip python3-setuptools aria2
-sudo dnf install python3 python3-pip python3-setuptools aria2
-sudo pacman -S python3 python3-pip python3-setuptools aria2
+sudo apt install python3 python3-pip python3-setuptools aria2 libxslt-dev
+sudo dnf install python3 python3-pip python3-setuptools aria2 libxslt-dev
+sudo pacman -S python3 python3-pip python3-setuptools aria2 libxslt-dev
 pip3 install validators pyquery
